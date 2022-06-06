2b3fc6a9db00e768d68202766771cd62e65e491e
vishnu244 <vishnunali244@gmail.com>
2022-06-06 12:08:51 +0530

    [Vishnu]-Loading indian state Census and count the Number od Rows

--- IndianStatesCensusAnalyser/Program.cs
+++ IndianStatesCensusAnalyser/Program.cs
@@ -10,15 +10,19 @@ string IndianStateCensusDataWrongExtensionFilePath = @"C:\Users\Admin\Desktop\Vi
 string IndianStateCensusDataWrongFilePath = @"C:\Users\Admin\Desktop\Vishnu\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndianStateCensusData.csv";
 string csvPath = @"C:\Users\Admin\Desktop\Vishnu\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusData.csv";
 string DelimiterIndianStateCensusDataFilePath = @"C:\Users\Admin\Desktop\Vishnu\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\DelimiterIndiaStateCensusData.csv";
+string IndiaStateCodeCsvFilePath = @"C:\Users\Admin\Desktop\Vishnu\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCode.csv";
+
 
 //censusAnalyser.ReadCsvFile();
 string IndianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
 string IndianStateCensusHeaders2 = "States,population,areaInSqKm,densityPerSqKm";
+string IndiaStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
 
 
 while (true)
 {
-    Console.WriteLine("Please choose the option: \n1)UC1 - Check the number of records in CSV file\n2)UC1.2 - Given wrong file Path\n3)UC1.3 - Giving wrong text file as Input\n4)UC1.4-Throw exception if Delimeter is Wrong\n5)UC1.5-Throw exception if Header is Wrong");
+    Console.WriteLine("Please choose the option: \n1)UC1 - Check the number of records in CSV file\n2)UC1.2 - Given wrong file Path\n3)UC1.3 - Giving wrong text file as Input\n4)UC1.4-Throw exception if Delimeter is Wrong\n5)UC1.5-Throw exception " +
+        "if Header is Wrong\n6)UC2 Load India State code (Count Rows)");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -66,6 +70,10 @@ while (true)
             {
                 Console.WriteLine(e.Message);
             }
+            break;
+        case 6:
+            Dictionary<string, CensusDTO> stateRecord = censusAnalyser.LoadCensusData(Country.INDIA, IndiaStateCodeCsvFilePath, IndiaStateCodeHeaders);
+            Console.WriteLine(stateRecord.Count);
             break;
         default:
             Console.WriteLine("Please choose Valid option!");
