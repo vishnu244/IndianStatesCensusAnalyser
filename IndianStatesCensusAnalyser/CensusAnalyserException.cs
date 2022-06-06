b07c343190402f89cce3e929819153da83fd29e3
vishnu244 <vishnunali244@gmail.com>
2022-06-06 10:48:35 +0530

    [Vishnu]-Loading Indian State Census Information

---
+++ IndianStatesCensusAnalyser/CensusAnalyserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND,
+            INVALID_FILE_TYPE,
+            INCORRECT_DELIMITER,
+            INCORRECT_HEADER,
+            NO_SUCH_COUNTRY
+        }
+        public ExceptionType eType;
+        public CensusAnalyserException(string message, ExceptionType exceptionType) : base(message)
+        {
+            this.eType = exceptionType;
+        }
+    }
+}
--- IndianStatesCensusAnalyser/Program.cs
+++ IndianStatesCensusAnalyser/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using IndianStatesCensusAnalyser;
+using IndianStatesCensusAnalyser.POCO;
+using static IndianStatesCensusAnalyser.CensusAnalyser;
+
 Console.WriteLine("Welcome to Indian States Analyzer Problem!");
+
+CensusAnalyser censusAnalyser = new();
+string csvPath = @"C:\Users\Admin\Desktop\Vishnu\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusData.csv";
+//censusAnalyser.ReadCsvFile();
+string IndianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+Dictionary<string, CensusDTO> totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, csvPath, IndianStateCensusHeaders);
+Console.WriteLine(totalRecord.Count);
